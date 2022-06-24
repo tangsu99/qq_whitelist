9ee48e31db6416224ea4b12ce354b9a7bf54fac2
徐腾达 <2560561868@qq.com>
2022-06-24 20:05:18 +0800

    Init

---
+++ qq_whitelist/__init__.py
@@ -0,0 +1,184 @@
+import json
+import websocket
+from mcdreforged.api.all import *
+from mcdreforged.api.rcon import RconConnection
+
+config = {
+    'command_prefix': '!!wl',
+    'servers': [
+        {
+            'server_name': 'Creative',
+            'rcon_address': '127.0.0.1',
+            'rcon_port': 25575,
+            'rcon_password': 'PASSWORD'},
+        {
+            'server_name': 'Mirror',
+            'rcon_address': '127.0.0.1',
+            'rcon_port': 25576,
+            'rcon_password': 'PASSWORD'
+        }
+    ],
+    'convert_vanilla_whitelist': False,
+    'convert_lls_whitelist': False,
+    'ws_address': '127.0.0.1',
+    'ws_port': 6700,
+    'access_token': 'ACCESSTOKEN',
+    'group_id': 000
+}
+default_config = config.copy()
+
+whitelist = {
+    "uuid": {
+        "name": "tangsu",
+        "qq": "100001",
+        "whitelist": False
+    }
+}
+
+
+
+class websocket(websocket.WebSocketApp):
+    ...
+
+
+def whfl ():
+    wh = open('./config/qq_whitelist/whitelist.json', 'r')
+    wh = json.loads(wh.read())
+    return wh
+
+
+def help_messag(src: CommandSource):
+    src.reply('''{0} add <player> <qq_id>:添加白名单。
+{0} remove player <player>:通过玩家ID移除白名单。
+{0} remove qq <player>:通过玩家绑定的qq账号移除白名单。
+{0} list:查看白名单列表。'''.format(config.get('command_prefix')))
+
+
+def open_whitelist(whitelist_file):
+    whitelist = open(whitelist_file, "r")
+    whitelist = json.loads(whitelist.read())
+    return whitelist
+
+
+def convert_vanilla_whitelist(ctx):
+    whitelist = open_whitelist("./server/whitelist.json")
+    for i in whitelist:
+        print(i['name'])
+
+
+def convert_lls_whitelist(ctx):
+    whitelist = open_whitelist("./config/qq_whitelist/whitelist.json")
+    # print(whitelist)
+
+
+def rcon_reload_command(server: ServerInterface, src):
+    rcon_server = config['servers']
+    src.reply('[DEBUG] {0}'.format(rcon_server))
+
+    for i in rcon_server:
+        address = i['rcon_address']
+        port = i['rcon_port']
+        password = i['rcon_password']
+        server_name = i['server_name']
+        rcon = RconConnection(address, port, password)
+        try:
+            t = rcon.connect()
+        except:
+            src.reply('{0} 无法连接,请检查 config'.format(server_name))
+        if t:
+            reply = rcon.send_command('whitelist reload')
+            src.reply('{0} 已同步\n{0}:{1}'.format(server_name, reply))
+            rcon.disconnect()
+
+
+def wlist():
+    wlist = []
+    for i in whitelist:
+        wlist.append(whitelist[i]['name'])
+    return wlist
+
+
+class Command:
+    def __init__(self, src: CommandSource, server: PluginServerInterface):
+        self.src = src
+        self.server = server
+
+    def whitelist_add(self, ctx):
+        self.src.reply(ctx)
+
+    def whitelist_remove_player(self, ctx):
+        self.src.reply(ctx)
+
+    def whitelist_remove_qq(self, ctx):
+        self.src.reply(ctx)
+
+    def whitelist_list(self, whitelist):
+        #self.src.reply()
+        self.src.reply(wlist())
+
+    def sync_whitelist(self):
+        rcon_reload_command(self.server, self.src)
+
+
+def on_load(server: PluginServerInterface, prev_module):
+    global config
+    config = server.load_config_simple('config.json', default_config)
+
+    global whitelist
+    whitelist = whfl()
+
+    # rcon_command(server, config, ['tansgu99','1','2'])#debug
+
+    server.register_command(
+        Literal(config['command_prefix']).runs(lambda src: help_messag(src)).
+            then(
+            Literal('add').
+                then(
+                Text('player_name').
+                    then(
+                    Text('qq').runs(lambda src, ctx: Command(src, server).whitelist_add(ctx))
+                )
+            )
+
+        ).
+            then(
+            Literal('remove').
+                then(
+                Literal('player').
+                    then(
+                    Text('player_name').
+                        runs(lambda src, ctx: Command(src, server).whitelist_remove_player(ctx))
+                )
+            ).
+                then(
+                Literal('qq').
+                    then(
+                    Integer('qq').
+                        runs(lambda src, ctx: Command(src, server).whitelist_remove_qq(ctx))
+                )
+            )
+        ).
+            then(
+            Literal('list').
+                runs(lambda src: Command(src, server).whitelist_list(whitelist))
+        ).
+            then(
+            Literal('sync').
+                runs(lambda src: Command(src, server).sync_whitelist())
+        ).
+            then(
+            Literal('wlc').
+                then(
+                Literal('vanilla').
+                    runs(lambda ctx: convert_vanilla_whitelist(ctx))
+            ).
+                then(
+                Literal('lls').
+                    runs(lambda ctx: convert_lls_whitelist(ctx))
+            )
+        )
+    )
+
+
+def on_unload(server: PluginServerInterface):
+    ...
---
+++ qq_whitelist/websockets.py
@@ -0,0 +1,10 @@
+import websocket
+
+
+def ws(websockets):
+    websockets.connect('ws://10.10.6.91:5678')
+    websockets.connect
+
+
+if __name__ == '__main__':
+    ...
